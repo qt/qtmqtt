13a98e39e7a993fd51d8814d4c46dc283b068aa4
Ivan Solovev <ivan.solovev@qt.io>
2025-07-23 17:39:48 +0200

    Mark files that implement MQTT protocol as security-critical
    The qmqttconnection.cpp file implements parts of the MQTT protocol,
    including proper packet generation
    (e.g. in QMqttConnection::sendControlConnect()) and parsing
    (e.g. in QMqttConnection::processDataHelper()).
    
    The qmqttcontrolpacket.cpp file implements payload serialization
    in QMqttControlPacket::serializePayload().
    
    Task-number: QTBUG-135764
    Pick-to: 6.10 6.9 6.8
    Change-Id: I5b398c8739e549ba849fcc31f25cd05ca3384f30
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>

--- src/mqtt/qmqttcontrolpacket.cpp
+++ src/mqtt/qmqttcontrolpacket.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2017 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// Qt-Security score:critical reason:network-protocol
 
 #include "qmqttcontrolpacket_p.h"
 
