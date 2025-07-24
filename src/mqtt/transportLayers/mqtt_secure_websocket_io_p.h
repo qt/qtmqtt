3d4c43d9c99d240b0b0a06629db99e2fbe8a5f8a
Ivan Solovev <ivan.solovev@qt.io>
2025-07-24 10:42:17 +0200

    Mark the rest of sources as security-significant
    This is the default level. All critical parts were identified in the
    previous commits.
    
    Fixes: QTBUG-135764
    Pick-to: 6.9 6.8
    Change-Id: Ib9df3db37e3efe3074f0fbc99eb60d31281d5a5a
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit 18548033e15a4f5952065fe5ba6cd764b4e356a2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/mqtt/transportLayers/mqtt_secure_websocket_io_p.h
+++ src/mqtt/transportLayers/mqtt_secure_websocket_io_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QTMQTTCLIENT_SECURE_WEBSOCKET_IODEVICE_H
 #define QTMQTTCLIENT_SECURE_WEBSOCKET_IODEVICE_H
